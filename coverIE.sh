1d2c9f105a71ac8d08f27edfa6edc7204ac96dcd
stevec12 <95181013+stevec12@users.noreply.github.com>
2022-03-26 12:08:03 -0400

    Generate IEs via Polymake

---
+++ coverIE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Uses setcover to generate a cover and outputs IEs from polymake
+# usage: coverIE filename-stem
+if [ ${#} -lt 3 ] ; then
+	echo 'usage: $coverIE |Universe| total_sets filename-stem';
+	exit 1;
+fi
+echo "${1} ${2} y ${3}" | ./setcover
+#Now have file filename-stem.txt as suitable script file for polymake
+polymake --script ${3}.txt
